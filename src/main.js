4e0253d35ba467e053fbf356e93234334aa6e534
Issa <badosa2@yahoo.fr>
2025-09-11 23:48:38 +0000

    configuration de firebase

--- src/main.js
+++ src/main.js
@@ -15,6 +15,25 @@ import { fab } from '@fortawesome/free-brands-svg-icons'
 // Le dark mode s'initialise automatiquement dans le composable
 import './composables/useSimpleDarkMode'
 
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBiP4zykrTNFQd8gxaY-qQcVuWCWIGwjL0",
+  authDomain: "repae-f13cc.firebaseapp.com",
+  projectId: "repae-f13cc",
+  storageBucket: "repae-f13cc.firebasestorage.app",
+  messagingSenderId: "547740336954",
+  appId: "1:547740336954:web:1e9612f40c1e6919474603"
+};
+
+// Initialize Firebase
+const appFirebase = initializeApp(firebaseConfig);
+
 library.add(fas, far, fab)
 
 const app = createApp(App)
